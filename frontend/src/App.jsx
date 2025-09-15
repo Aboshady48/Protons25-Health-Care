7c2819cdc3a7fadb11353e24bf1ec074e6ace25f
Nada-Fahmy3 <nadamohamedfahmy17@gmail.com>
2025-09-16 01:11:33 +0300

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import {  BrowserRouter, Routes, Route, Link, Router  } from "react-router-dom";
+import Register from "./componets/register.jsx";
+import Login from "./componets/login.jsx";
+import index from"./style/index.css";
+import React from "react";
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+  <div>
+   <h1>hello</h1>
+  </div>
   );
 }
 
-export default App
+export default App;
+  // <BrowserRouter>
+    
+  //     <Routes>
+      
+  //       <Route path="/Register" element={<Register />} />
+  //       <Route path="/Login" element={<Login/>} />
+  //     </Routes>
+  //   </BrowserRouter>
+ 
