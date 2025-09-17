89db412ad203b205d4fb93ac782e50514b965e4c
Aboshady <aboshady48585@gmail.com>
2025-09-18 00:35:18 +0300

    test

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import {useState} from 'react'
 
 export const Home = () => {
+  const [count,setCount] = useState(0)
+
+
   return (
     <div>
         <h1>Home</h1>
         <p>This is the home page</p>
+        <button onClick={()=>setCount(count+1)}>count +</button>
+        
+        but
+        <h1>{count}</h1>
     </div>
   )
 }
