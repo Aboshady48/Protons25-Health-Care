e3a0e0f539b65b395dd5f35e22e7fa7639e7e99f
Nada-Fahmy3 <nadamohamedfahmy17@gmail.com>
2025-09-18 16:37:49 +0300

    i had finshed the get task by id

---
+++ frontend/src/components/dailyPlanner/DeleteTaskBy.jsx
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import {useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+export const DeleteTasks = ()=>{
+    //http://localhost:5000/api/tasks/${id}
+    
+}
